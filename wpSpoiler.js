262ef85ebff256f826cb306f42aca0ded0711ea0
Felix Triller <felixtriller@me.com>
2011-02-20 00:28:03 +0100

    developing

--- wpSpoiler.js
+++ wpSpoiler.js
@@ -1,4 +1,6 @@
 jQuery(document).ready(function() {
+	jQuery(".spoiler-text").hide();
+	
 	jQuery(".spoiler-link").click(function () {
 		jQuery(this).toggleClass("show hide");
 		jQuery(this).next(".spoiler-text").toggle("fast");
